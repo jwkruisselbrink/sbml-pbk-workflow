b20d3a1c25f23de4c046a10358850e591752f989
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-06-23 13:06:23 +0200

    Create separate SBML element infos class

--- src/export_sbml_element_infos.py
+++ src/export_sbml_element_infos.py
@@ -3,9 +3,7 @@ import argparse
 import libsbml as ls
 from pathlib import Path
 import traceback
-import pandas as pd
-from unit_definitions import UnitDefinitions
-from term_definitions import TermDefinitions
+from sbml_element_infos_exporter import sbmlElementInfosExporter
 
 def main():
     """Creates an annotation template for the provided SBML file."""
@@ -33,7 +31,8 @@ def main():
         sys.exit(1)
 
     # Create terms datatable
-    df = exportTerms(model)
+    infosExporter = sbmlElementInfosExporter()
+    df = infosExporter.exportTerms(model)
 
     # Write data table to csv file
     if not f_out.exists() or args.force:
@@ -42,150 +41,6 @@ def main():
     else:
         print(f'{f_out} already exists, use -f to force conversion')
 
-def getUnitString(element):
-    """Tries to get the (UCUM formated) unit string of the specified element."""
-    unit = ls.UnitDefinition.printUnits(element.getDerivedUnitDefinition())
-    unit = element.getUnits()
-    if (unit):
-        for index, value in enumerate(UnitDefinitions):
-            if unit.lower() == value['id'].lower() \
-                or any(val.lower() == unit.lower() for val in value['common_ids']):
-                return value['UCUM'] if value['UCUM'] else value['id']
-    return ""
-
-def getResourceDefinition(element, element_type):
-    """Tries to find a resource definition for the specified element."""
-    element_id = element.getId()
-    for index, value in enumerate(TermDefinitions):
-        if value['element_type'] == element_type:
-            if 'recommended_id' in value.keys() \
-                and element_id.lower() == value['recommended_id'].lower():
-                return value
-            elif 'common_ids' in value.keys() \
-                and any(element_id.lower() == val.lower() for val in value['common_ids']):
-                return value
-    return None
-
-def getTerm(element):
-    """Helper function to extract is-a resource URI."""
-    cvTerms = element.getCVTerms()
-    if cvTerms:
-        # Check if there already is an annotation for the element
-        for term in cvTerms:
-            num_resources = term.getNumResources()
-            for j in range(num_resources):
-                if term.getQualifierType() == ls.BIOLOGICAL_QUALIFIER and \
-                    term.getBiologicalQualifierType() == ls.BQB_IS:
-                    return term.getResourceURI(j)
-
-    return None 
-
-def exportTerms(model):
-    dt = []
-    dt_compartments = getCompartmentTerms(model)
-    dt.extend(dt_compartments)
-    dt_species = getSpeciesTerms(model)
-    dt.extend(dt_species)
-    dt_parameters = getParameterTerms(model)
-    dt.extend(dt_parameters)
-    terms = pd.DataFrame(
-        dt,
-        columns=["element_id", "sbml_type", "element_description", "unit", "URI", "description", "remark"]
-    )
-    return terms
-
-def getCompartmentTerms(model):
-    element_type="compartment"
-    dt = []
-    for i in range(0,model.getNumCompartments()):
-        element = model.getCompartment(i)
-
-        name = element.getName()
-        description = ''
-        uri = getTerm(element)
-
-        # Try to find resource definition for element
-        resource = getResourceDefinition(element, element_type)
-        if (resource is not None):
-            if 'resources' in resource.keys() and len(resource['resources']) > 0:
-                uri = resource['resources'][0]['URI']
-            if 'name' in resource.keys():
-                name = resource['name']
-            if 'description' in resource.keys():
-                description = resource['description']
-
-        dt.append([
-            element.getId(),
-            element_type,
-            name,
-            getUnitString(element),
-            uri,
-            description,
-            ""
-        ])
-    return dt
-
-def getSpeciesTerms(model):
-    element_type="species"
-    dt = []
-    for i in range(0,model.getNumSpecies()):
-        element = model.getSpecies(i)
-
-        name = element.getName()
-        description = ''
-        uri = getTerm(element)
-
-        # Try to find resource definition for element
-        resource = getResourceDefinition(element, element_type)
-        if (resource is not None):
-            if 'resources' in resource.keys():
-                uri = resource['resources'][0]['URI']
-            if 'name' in resource.keys():
-                name = resource['name']
-            if 'description' in resource.keys():
-                description = resource['description']
-
-        dt.append([
-            element.getId(),
-            element_type,
-            name,
-            getUnitString(element),
-            uri,
-            description,
-            ""
-        ])
-    return dt
-
-def getParameterTerms(model):
-    element_type="parameter"
-    dt = []
-    for i in range(0,model.getNumParameters()):
-        element = model.getParameter(i)
-
-        name = element.getName()
-        description = ''
-        uri = getTerm(element)
-
-        # Try to find resource definition for element
-        resource = getResourceDefinition(element, element_type)
-        if (resource is not None):
-            if 'resources' in resource.keys():
-                uri = resource['resources'][0]['URI']
-            if 'name' in resource.keys():
-                name = resource['name']
-            if 'description' in resource.keys():
-                description = resource['description']
-
-        dt.append([
-            element.getId(),
-            element_type,
-            name,
-            getUnitString(element),
-            uri,
-            description,
-            ""
-        ])
-    return dt
 
 if __name__ == "__main__":
     main()
---
+++ src/sbml_element_infos_exporter.py
@@ -0,0 +1,151 @@
+import libsbml as ls
+import pandas as pd
+from unit_definitions import UnitDefinitions
+from term_definitions import TermDefinitions
+
+class sbmlElementInfosExporter:
+
+    def exportTerms(self, model):
+        dt = []
+        dt_compartments = self.getCompartmentTerms(model)
+        dt.extend(dt_compartments)
+        dt_species = self.getSpeciesTerms(model)
+        dt.extend(dt_species)
+        dt_parameters = self.getParameterTerms(model)
+        dt.extend(dt_parameters)
+        terms = pd.DataFrame(
+            dt,
+            columns=["element_id", "sbml_type", "element_description", "unit", "URI", "description", "remark"]
+        )
+        return terms
+
+    def getCompartmentTerms(self, model):
+        element_type="compartment"
+        dt = []
+        for i in range(0,model.getNumCompartments()):
+            element = model.getCompartment(i)
+
+            name = element.getName()
+            description = ''
+            uri = self.getTerm(element)
+
+            # Try to find resource definition for element
+            resource = self.getResourceDefinition(element, element_type)
+            if (resource is not None):
+                if 'resources' in resource.keys() and len(resource['resources']) > 0:
+                    uri = resource['resources'][0]['URI']
+                if 'name' in resource.keys():
+                    name = resource['name']
+                if 'description' in resource.keys():
+                    description = resource['description']
+
+            dt.append([
+                element.getId(),
+                element_type,
+                name,
+                self.getUnitString(element),
+                uri,
+                description,
+                ""
+            ])
+        return dt
+
+    def getSpeciesTerms(self, model):
+        element_type="species"
+        dt = []
+        for i in range(0,model.getNumSpecies()):
+            element = model.getSpecies(i)
+
+            name = element.getName()
+            description = ''
+            uri = self.getTerm(element)
+
+            # Try to find resource definition for element
+            resource = self.getResourceDefinition(element, element_type)
+            if (resource is not None):
+                if 'resources' in resource.keys() and len(resource['resources']) > 0:
+                    uri = resource['resources'][0]['URI']
+                if 'name' in resource.keys():
+                    name = resource['name']
+                if 'description' in resource.keys():
+                    description = resource['description']
+
+            dt.append([
+                element.getId(),
+                element_type,
+                name,
+                self.getUnitString(element),
+                uri,
+                description,
+                ""
+            ])
+        return dt
+
+    def getParameterTerms(self, model):
+        element_type="parameter"
+        dt = []
+        for i in range(0,model.getNumParameters()):
+            element = model.getParameter(i)
+
+            name = element.getName()
+            description = ''
+            uri = self.getTerm(element)
+
+            # Try to find resource definition for element
+            resource = self.getResourceDefinition(element, element_type)
+            if (resource is not None):
+                if 'resources' in resource.keys() and len(resource['resources']) > 0:
+                    uri = resource['resources'][0]['URI']
+                if 'name' in resource.keys():
+                    name = resource['name']
+                if 'description' in resource.keys():
+                    description = resource['description']
+
+            dt.append([
+                element.getId(),
+                element_type,
+                name,
+                self.getUnitString(element),
+                uri,
+                description,
+                ""
+            ])
+        return dt
+
+    def getResourceDefinition(self, element, element_type):
+        """Tries to find a resource definition for the specified element."""
+        element_id = element.getId()
+        for index, value in enumerate(TermDefinitions):
+            if value['element_type'] == element_type:
+                if 'recommended_id' in value.keys() \
+                    and element_id.lower() == value['recommended_id'].lower():
+                    return value
+                elif 'common_ids' in value.keys() \
+                    and any(element_id.lower() == val.lower() for val in value['common_ids']):
+                    return value
+        return None
+
+    def getUnitString(self, element):
+        """Tries to get the (UCUM formated) unit string of the specified element."""
+        unit = ls.UnitDefinition.printUnits(element.getDerivedUnitDefinition())
+        unit = element.getUnits()
+        if (unit):
+            for index, value in enumerate(UnitDefinitions):
+                if unit.lower() == value['id'].lower() \
+                    or any(val.lower() == unit.lower() for val in value['common_ids']):
+                    return value['UCUM'] if value['UCUM'] else value['id']
+        return ""
+
+    def getTerm(self, element):
+        """Helper function to extract is-a resource URI."""
+        cvTerms = element.getCVTerms()
+        if cvTerms:
+            # Check if there already is an annotation for the element
+            for term in cvTerms:
+                num_resources = term.getNumResources()
+                for j in range(num_resources):
+                    if term.getQualifierType() == ls.BIOLOGICAL_QUALIFIER and \
+                        term.getBiologicalQualifierType() == ls.BQB_IS:
+                        return term.getResourceURI(j)
+
+        return None 
