114dc0815c3476332a47207c3d780bea101c7815
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-12-11 23:19:28 +0100

    Remove deprecated build_all script

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-python "$parent_path"/src/ant2sbml.py model/$1.ant -f
-
-python "$parent_path"/src/annotate_sbml.py model/$1.sbml model/$1.annotations.csv model/$1.sbml -f
-
-python "$parent_path"/src/validate_sbml.py model/$1.sbml
