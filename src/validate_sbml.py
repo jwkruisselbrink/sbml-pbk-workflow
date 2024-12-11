c939b121d30c7c067413d05b9600310bef404338
Johannes Kruisselbrink <jwkruisselbrink@gmail.com>
2024-12-11 17:46:37 +0100

    Fix build workflow

--- src/validate_sbml.py
+++ src/validate_sbml.py
@@ -22,19 +22,19 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Validate SBML file")
     parser.add_argument("f_in", help="Path to the SBML file")
-    parser.add_argument("log_file", help="Path to log file")
+    parser.add_argument("-l", "--log", type=str, help="Path to log file")
     args = parser.parse_args()
     f_in = Path(args.f_in)
     if not f_in.is_file():
         raise FileNotFoundError(f'File {f_in} does not exist.')
-    if args.log_file is not None:
+    if args.log is not None:
         # Check if output folder exists, otherwise create it
-        out_dir = os.path.dirname(args.log_file)
+        out_dir = os.path.dirname(args.log)
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
         # Create file logger
-        logger = create_file_logger(Path(args.log_file))
+        logger = create_file_logger(Path(args.log))
     else:
         logger = get_logger(__name__)
 
@@ -42,4 +42,4 @@ def main():
     validator.validate(f_in, logger)
 
 if __name__ == '__main__':
-  main(sys.argv) 
+  main() 
--- tests/test_validate_sbml.py
+++ tests/test_validate_sbml.py
@@ -6,11 +6,15 @@ from unittest.mock import patch
 
 class TestValidateSbml(TestBase):
 
-    @patch('sys.argv', ['ant2sbml.py', './tests/models/simple.annotated.sbml', './tests/output/simple.annotated.validation.log'])
-    def test_simple(self):
+    @patch('sys.argv', ['ant2sbml.py', './tests/models/simple.annotated.sbml', '-l', './tests/output/simple.annotated.validation.log'])
+    def test_simple_log_file(self):
         src.validate_sbml.main()
         path = pl.Path('./tests/output/simple.annotated.validation.log')
         self.assertIsFile(path)
+
+    @patch('sys.argv', ['ant2sbml.py', './tests/models/simple.annotated.sbml'])
+    def test_simple_log_console(self):
+        src.validate_sbml.main()
 
 if __name__ == '__main__':
     unittest.main()
